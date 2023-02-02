db4052a3d94ce81df1719d3bef75ed4e76152073
fmagoge <fmagoge@gmail.com>
2023-02-03 00:38:05 +0200

    - added Item activity

---
+++ app/src/main/java/mad/app/madandroidtestsolutions/ItemActivity.kt
@@ -0,0 +1,11 @@
+package mad.app.madandroidtestsolutions
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ItemActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_item)
+    }
+}
